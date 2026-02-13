d64c35631680b96685913201b9ca26b88a1f140e
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2026-02-13 17:44:55 -0300

    Update urls.py

---
+++ tarefas/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista_tarefas, name='lista_tarefas'),
+    path('criar/', views.criar_tarefa, name='criar_tarefa'),
+    path('concluir/<int:id>/', views.concluir_tarefa, name='concluir_tarefa'),
+    path('deletar/<int:id>/', views.deletar_tarefa, name='deletar_tarefa'),
+]
