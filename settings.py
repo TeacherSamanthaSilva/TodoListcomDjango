b07683dc01be988e1a072f70b6ea21d8d386b7bd
TeacherSamanthaSilva <75630998+TeacherSamanthaSilva@users.noreply.github.com>
2026-02-13 17:42:54 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -8,3 +8,21 @@ INSTALLED_APPS = [
 
     'tarefas',
 ]
+
+import os
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
